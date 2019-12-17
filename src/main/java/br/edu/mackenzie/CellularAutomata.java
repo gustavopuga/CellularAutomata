0d51ed0cb3690378ae1fd8f44a7de347881f329e
Gustavo Fortunato Puga <gfpuga@defensoria.sp.gov.br>
2019-12-17 09:32:58 -0300

    Improvement to aplly rule

--- src/main/java/br/edu/mackenzie/CellularAutomata.java
+++ src/main/java/br/edu/mackenzie/CellularAutomata.java
@@ -7,129 +7,140 @@ import java.util.Map;
 
 public class CellularAutomata {
 
-	private PopulationState[][] currentGeneration;
-	private List<PopulationState[][]> generations;
+    private PopulationState[][] currentGeneration;
+    private List<PopulationState[][]> generations;
 
-	private int columns;
-	private int rows;
-	private int population;
+    private int columns;
+    private int rows;
+    private int population;
 
-	public CellularAutomata(int columns, int rows) {
+    public CellularAutomata(int columns, int rows) {
 
-		this.generations = new ArrayList<>();
-		this.currentGeneration = new PopulationState[columns][rows];
-		this.columns = columns;
-		this.rows = rows;
-		this.population = columns * rows;
-		init();
+	this.generations = new ArrayList<>();
+	this.currentGeneration = new PopulationState[columns][rows];
+	this.columns = columns;
+	this.rows = rows;
+	this.population = columns * rows;
+	init();
+    }
+
+    private void init() {
+
+	Map<PopulationState, Double> populationStateQuantity = new HashMap<>();
+	for (PopulationState populationState : PopulationState.values()) {
+	    Double populationPercentage = populationState.getPopulationPercentage();
+	    Double maxPopulationState = null;
+	    if (populationPercentage != null) {
+		maxPopulationState = populationPercentage * population;
+	    }
+	    populationStateQuantity.put(populationState, maxPopulationState);
 	}
 
-	private void init() {
+	for (int i = 0; i < columns; i++) {
+	    for (int j = 0; j < rows; j++) {
 
-		Map<PopulationState, Double> populationStateQuantity = new HashMap<>();
-		for (PopulationState populationState : PopulationState.values()) {
-			Double populationPercentage = populationState.getPopulationPercentage();
-			Double maxPopulationState = null;
-			if (populationPercentage != null) {
-				maxPopulationState = populationPercentage * population;
+		PopulationState populationState = PopulationState.get((int) Math.floor((Math.random() * 3)));
+		Double quantity = populationStateQuantity.get(populationState);
+		if (quantity != null) {
+		    while (quantity <= 0) {
+			populationState = PopulationState.get((int) Math.floor((Math.random() * 3)));
+			quantity = populationStateQuantity.get(populationState);
+		    }
+		    populationStateQuantity.put(populationState, --quantity);
+		}
+		currentGeneration[i][j] = populationState;
+	    }
+	}
+    }
+
+    private void nextGeneration() {
+
+	PopulationState[][] nextGeneration = new PopulationState[columns][rows];
+	this.generations.add(this.currentGeneration);
+
+	// Loop through every spot in our 2D array and check spots neighbors
+	for (int x = 0; x < columns; x++) {
+	    for (int y = 0; y < rows; y++) {
+
+		// Add up all the states in a 3x3 surrounding grid
+		/*int neighborhood = 0;
+		for (int i = -1; i <= 1; i++) {
+		    for (int j = -1; j <= 1; j++) {
+			neighborhood += this.currentGeneration[(x + i + this.columns)
+				% this.columns][(y + j + this.rows) % this.rows].getValue();
+		    }
+		}*/
+
+		List<PopulationState> neighborhood = new ArrayList<>();
+		for (int i = -1; i <= 1; i++) {
+		    for (int j = -1; j <= 1; j++) {
+			int xNeighborhood = (x + i + this.columns) % this.columns;
+			int yNeighborhood = (y + j + this.rows) % this.rows;
+			if (x != xNeighborhood && yNeighborhood != y) {
+			    neighborhood.add(this.currentGeneration[xNeighborhood][yNeighborhood]);
 			}
-			populationStateQuantity.put(populationState, maxPopulationState);
+		    }
 		}
+		
+		// Subtract the current cell's state since
+		PopulationState cell = currentGeneration[x][y];
+		//neighborhood -= cell.getValue();
+		nextGeneration[x][y] = cell.applyRule(neighborhood);
 
-		for (int i = 0; i < columns; i++) {
-			for (int j = 0; j < rows; j++) {
-
-				PopulationState populationState = PopulationState.get((int) Math.floor((Math.random() * 3)));
-				Double quantity = populationStateQuantity.get(populationState);
-				if (quantity != null) {
-					while (quantity <= 0) {
-						populationState = PopulationState.get((int) Math.floor((Math.random() * 3)));
-						quantity = populationStateQuantity.get(populationState);
-					}
-					populationStateQuantity.put(populationState, --quantity);
-				}
-				currentGeneration[i][j] = populationState;
-			}
-		}
+	    }
 	}
 
-	private void nextGeneration() {
+	currentGeneration = nextGeneration;
+    }
 
-		PopulationState[][] nextGeneration = new PopulationState[columns][rows];
-		this.generations.add(this.currentGeneration);
+    public void nextGeneration(int time) {
 
-		// Loop through every spot in our 2D array and check spots neighbors
-		for (int x = 0; x < columns; x++) {
-			for (int y = 0; y < rows; y++) {
+	for (int i = 0; i < time; i++) {
+	    nextGeneration();
+	}
+    }
 
-				// Add up all the states in a 3x3 surrounding grid
-				int neighborhood = 0;
-				for (int i = -1; i <= 1; i++) {
-					for (int j = -1; j <= 1; j++) {
-						neighborhood += this.currentGeneration[(x + i + this.columns)
-								% this.columns][(y + j + this.rows) % this.rows].getValue();
-					}
-				}
+    public Map<PopulationState, List<Double>> generateGenerationsStateMap() {
 
-				// Subtract the current cell's state since
-				PopulationState cell = currentGeneration[x][y];
-				neighborhood -= cell.getValue();
-				nextGeneration[x][y] = cell.applyRule(neighborhood);
+	Map<PopulationState, List<Double>> map = new HashMap<>();
+	map.put(PopulationState.SUSCEPTIBLE, new ArrayList<>());
+	map.put(PopulationState.INFECTIOUS, new ArrayList<>());
+	map.put(PopulationState.RECOVERY, new ArrayList<>());
 
-			}
+	for (PopulationState[][] populationStates : generations) {
+
+	    double susceptible = 0;
+	    double infectious = 0;
+	    double recovery = 0;
+
+	    for (int x = 0; x < columns; x++) {
+		for (int y = 0; y < rows; y++) {
+		    PopulationState state = populationStates[x][y];
+
+		    switch (state) {
+
+		    case SUSCEPTIBLE:
+			susceptible++;
+			break;
+
+		    case INFECTIOUS:
+			infectious++;
+			break;
+
+		    case RECOVERY:
+			recovery++;
+			break;
+
+		    }
 		}
+	    }
 
-		currentGeneration = nextGeneration;
+	    map.get(PopulationState.SUSCEPTIBLE).add(susceptible);
+	    map.get(PopulationState.INFECTIOUS).add(infectious);
+	    map.get(PopulationState.RECOVERY).add(recovery);
+
 	}
 
-	public void nextGeneration(int time) {
-
-		for (int i = 0; i < time; i++) {
-			nextGeneration();
-		}
-	}
-
-	public Map<PopulationState, List<Double>> generateGenerationsStateMap() {
-
-		Map<PopulationState, List<Double>> map = new HashMap<>();
-		map.put(PopulationState.SUSCEPTIBLE, new ArrayList<>());
-		map.put(PopulationState.INFECTIOUS, new ArrayList<>());
-		map.put(PopulationState.RECOVERY, new ArrayList<>());
-
-		for (PopulationState[][] populationStates : generations) {
-
-			double susceptible = 0;
-			double infectious = 0;
-			double recovery = 0;
-
-			for (int x = 0; x < columns; x++) {
-				for (int y = 0; y < rows; y++) {
-					PopulationState state = populationStates[x][y];
-
-					switch (state) {
-
-					case SUSCEPTIBLE:
-						susceptible++;
-						break;
-
-					case INFECTIOUS:
-						infectious++;
-						break;
-
-					case RECOVERY:
-						recovery++;
-						break;
-
-					}
-				}
-			}
-
-			map.get(PopulationState.SUSCEPTIBLE).add(susceptible);
-			map.get(PopulationState.INFECTIOUS).add(infectious);
-			map.get(PopulationState.RECOVERY).add(recovery);
-
-		}
-
-		return map;
-	}
+	return map;
+    }
 }
--- src/main/java/br/edu/mackenzie/Main.java
+++ src/main/java/br/edu/mackenzie/Main.java
@@ -17,83 +17,77 @@ import org.knowm.xchart.style.Styler.LegendPosition;
 
 public class Main {
 
-	private static int columns = 400;
-	private static int rows = 400;
-	private static long normalize = columns * rows;
+    private static int columns = 200;
+    private static int rows = 200;
+    private static long normalize = columns * rows;
+
+    public static XYChart getChart(Map<PopulationState, List<Double>> map, int generations) {
+
+	// Create Chart
+	XYChart chart = new XYChartBuilder().width(800).height(600).title("População Sarampo").xAxisTitle("Tempo")
+		.yAxisTitle("População").theme(ChartTheme.Matlab).build();
+
+	// Customize Chart
+	chart.getStyler().setLegendPosition(LegendPosition.OutsideE);
+	chart.getStyler().setLegendVisible(true);
+	chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
+
+	double[] ySusceptible = null;
+	double[] yInfectious = null;
+	double[] yRecovery = null;
+	double[] xTime = IntStream.rangeClosed(1, generations).asDoubleStream().toArray();
+
+	for (PopulationState state : map.keySet()) {
+	    switch (state) {
+
+	    case SUSCEPTIBLE:
+		ySusceptible = map.get(state).stream().mapToDouble(i -> i / normalize).toArray();
+		break;
+
+	    case INFECTIOUS:
+		yInfectious = map.get(state).stream().mapToDouble(i -> i / normalize).toArray();
+		break;
+
+	    case RECOVERY:
+		yRecovery = map.get(state).stream().mapToDouble(i -> i / normalize).toArray();
+		break;
+
+	    }
+
+	}
+
+	Color blue = new Color(52, 152, 219);
+	Color red = new Color(231, 76, 60);
+	Color green = new Color(46, 204, 113);
+
+	chart.addSeries(PopulationState.SUSCEPTIBLE.getDescription(), xTime, ySusceptible).setMarkerColor(blue)
+		.setLineColor(blue);
+	chart.addSeries(PopulationState.INFECTIOUS.getDescription(), xTime, yInfectious).setMarkerColor(red)
+		.setLineColor(red);
+	chart.addSeries(PopulationState.RECOVERY.getDescription(), xTime, yRecovery).setMarkerColor(green)
+		.setLineColor(green);
+
+	return chart;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+	int time = 100;
+	CellularAutomata ca = new CellularAutomata(columns, rows);
+	ca.nextGeneration(time);
+
+	Map<PopulationState, List<Double>> map = ca.generateGenerationsStateMap();
+	XYChart chart = getChart(map, time);
+	new SwingWrapper<XYChart>(chart).displayChart();
+
+	/* BitmapEncoder.saveBitmap(chart, "./Sarampo_Chart", BitmapFormat.PNG); */
 	
-	public static XYChart getChart(Map<PopulationState, List<Double>> map, int generations) {
+	/*
+	 * BitmapEncoder.saveBitmapWithDPI(chart, "./Sarampo_Chart_300_DPI",
+	 * BitmapFormat.PNG, 300);
+	 */
+	 
 
-		// Create Chart
-		XYChart chart = new XYChartBuilder().width(800).height(600).title("População Sarampo").xAxisTitle("Tempo")
-				.yAxisTitle("População").theme(ChartTheme.Matlab).build();
-
-		// Customize Chart
-		chart.getStyler().setLegendPosition(LegendPosition.OutsideE);
-		chart.getStyler().setLegendVisible(true);
-		chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
-
-		double[] ySusceptible = null;
-		double[] yInfectious = null;
-		double[] yRecovery = null;
-		double[] xTime = IntStream.rangeClosed(1, generations).asDoubleStream().toArray();
-		
-		
-		for (PopulationState state : map.keySet()) {
-			switch (state) {
-
-			case SUSCEPTIBLE:
-				ySusceptible = map.get(state).stream().mapToDouble(i -> i/normalize).toArray();
-				break;
-
-			case INFECTIOUS:
-				yInfectious = map.get(state).stream().mapToDouble(i -> i/normalize).toArray();
-				break;
-
-			case RECOVERY:
-				yRecovery = map.get(state).stream().mapToDouble(i -> i/normalize).toArray();
-				break;
-
-			}
-
-		}
-
-		Color blue = new Color(52, 152, 219);
-		Color red = new Color(231, 76, 60);
-		Color green = new Color(46, 204, 113);
-
-		chart.addSeries(PopulationState.SUSCEPTIBLE.getDescription(), xTime, ySusceptible).setMarkerColor(blue)
-				.setLineColor(blue);
-		chart.addSeries(PopulationState.INFECTIOUS.getDescription(), xTime, yInfectious).setMarkerColor(red)
-				.setLineColor(red);
-		chart.addSeries(PopulationState.RECOVERY.getDescription(), xTime, yRecovery).setMarkerColor(green)
-				.setLineColor(green);
-
-		return chart;
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		
-		
-		  int time = 100; 
-		  CellularAutomata ca = new CellularAutomata(columns, rows);
-		  ca.nextGeneration(time);
-		  
-		  Map<PopulationState, List<Double>> map = ca.generateGenerationsStateMap();
-		  XYChart chart = getChart(map, time); new
-		  SwingWrapper<XYChart>(chart).displayChart();
-		 
-
-		/* BitmapEncoder.saveBitmap(chart, "./Sarampo_Chart", BitmapFormat.PNG); */ 
-		
-		
-		/*
-		 * BitmapEncoder.saveBitmapWithDPI(chart, "./Sarampo_Chart_300_DPI",
-		 * BitmapFormat.PNG, 300);
-		 */
-		 
-		 
-
-	}
+    }
 
 }
