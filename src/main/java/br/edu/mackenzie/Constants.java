21659ea1adc7c005a6469e5eacc128d6f6274eba
Gustavo Fortunato Puga <gfpuga@defensoria.sp.gov.br>
2019-12-18 18:48:40 -0300

    Adding constants file

---
+++ src/main/java/br/edu/mackenzie/Constants.java
@@ -0,0 +1,10 @@
+package br.edu.mackenzie;
+
+public class Constants {
+
+    public final static double K = 1;
+    public final static int COLUMNS = 200;
+    public final static int ROWS = 200;
+    public final static long NORMALIZE = COLUMNS * ROWS;
+    public final static int TIME = 100;
+}
